3f5be5352970569e962c41058c4ee175d2fafe6d
akshaysoftcode <57672893+akshaysoftcode@users.noreply.github.com>
2020-03-11 21:16:51 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,19 @@
+// maDE BY :  Akshay Mishra
+public class loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 0;
+		// while loop intializing
+		while (value < 6) {
+			System.out.println("while loop value" + value);
+			value += 1;
+		}
+		//for loop intialize
+		for(int i=0;i<10 ;i++)
+		{
+			System.out.println("for loop value"+ i);
+		}
+	}
+
+}
