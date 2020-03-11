5e618ce7255e2759fbdc04c9873629c1775d1cf4
akshaysoftcode <57672893+akshaysoftcode@users.noreply.github.com>
2020-03-11 20:49:25 +0530

    Add files via upload

---
+++ stringadd.java
@@ -0,0 +1,10 @@
+
+public class stringadd {
+	public static void main(String[] args) {
+		int rno= 20;
+		char section= 'b';
+		String name= "Akshay";
+		
+		System.out.println("roll no:"+ rno+" " +"name:"+ name+  " "+ "section:"+ section);
+	}
+}
